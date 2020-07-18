d5d54bb089348c0cc88bfb8ddb2723b3b01cfd54
MrTapa <tapata42@gmail.com>
2020-07-18 15:11:11 +0200

    Update Feedback.js

--- Feedback.js
+++ Feedback.js
@@ -1,9 +1,9 @@
 <script>
-    Userback = window.Userback || {};
-    Userback.access_token = '7239|11134|xtrebtiQ7zSeDG4K0UUYXcLeCHSn6E4Cb4JPVzHN3eskimxxas';
+    Userback = window.Userback  {};
+    Userback.access_token = '9405|17842|HQRUFxWbXpBzpaZ5ILVkqQZFYc4rQVB3yyLfeXwVyDphobnlLy';
     (function(id) {
         var s = document.createElement('script');
         s.async = 1;s.src = 'https://static.userback.io/widget/v1.js';
-        var parent_node = document.head || document.body;parent_node.appendChild(s);
+        var parent_node = document.head  document.body;parent_node.appendChild(s);
     })('userback-sdk');
 </script>
